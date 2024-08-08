bdc55bcdd29533a00e2b6b5a5023a168a0c230e1
MEIRTH MAULIDA H <meirthmh.mei@gmail.com>
2024-08-09 01:25:52 +0900

    feat: add new button reusable component in common folder

---
+++ src/common/button/button.tsx
@@ -0,0 +1,38 @@
+import { FC, ButtonHTMLAttributes, ReactNode, PropsWithChildren } from "react";
+
+export const enum ButtonStyles {
+  BaseButton = "border-none",
+  PrimaryButton = "bg-green-900 rounded-lg text-white p-2 my-5 font-semibold",
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  ButtonStyle?: ButtonStyles;
+  hidenButton?: boolean;
+  activeButton?: boolean;
+  isLoading?: boolean;
+}
+
+type ButtonCompnent = FC<ButtonProps> & PropsWithChildren;
+const Button: ButtonCompnent = ({
+  children,
+  ButtonStyle = "",
+  isLoading = false,
+  ...restProps
+}) => {
+  return (
+    <button
+      {...restProps}
+      disabled={isLoading}
+      className={
+        "hover:brightness-50 " +
+        ButtonStyle +
+        `${restProps.className ? " " + restProps.className : ""}`
+      }
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;

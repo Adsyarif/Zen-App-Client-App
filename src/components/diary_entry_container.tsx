c6229c07395eccacf4b933da05685c6c5a6765a6
MEIRTH MAULIDA H <meirthmh.mei@gmail.com>
2024-08-12 22:30:16 +0900

    feat: resolve conflict from diary_entry_page branch to main

---
+++ src/components/diary_entry_container.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import IconButton from "@mui/material/IconButton";
+import AddReactionOutlinedIcon from "@mui/icons-material/AddReactionOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import ZenZoneBrandLogo from "../../public/ZenZoneBrandPict.png";
+
+const DiaryEntryContainer: React.FC = () => {
+  const [diaryText, setDiaryText] = useState<string>("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDiaryText(event.target.value);
+  };
+
+  return (
+    <div className="bg-green-900 text-lg font-semibold text-center items-center rounded-lg p-6 h-screen">
+      <p className="text-white text-2xl mb-6">My Diary Entry</p>
+      <div className="bg-gray-100 rounded-lg shadow-md p-4 w-full h-full max-h-[80vh] overflow-y-auto">
+        <div className="flex justify-between">
+          <IconButton>
+            <AddReactionOutlinedIcon className="w-10 h-10 text-green-900" />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon className="w-10 h-10 text-green-900" />
+          </IconButton>
+        </div>
+        <p className="text-green-900 text-left font-bold py-2">
+          September 17th, 2020
+        </p>
+        <p className="text-green-900 text-left font-bold py-2">
+          My current mood:
+        </p>
+        <div className="relative bg-white rounded-lg p-4">
+          <div
+            className="absolute inset-0"
+            style={{
+              backgroundImage: `url(${ZenZoneBrandLogo.src})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              opacity: 0.1,
+              zIndex: 0,
+            }}
+          />
+          <textarea
+            className="w-full h-96 p-2 border-none outline-none bg-transparent resize-none text-black relative z-10 placeholder:text-center placeholder:text-slate-700"
+            placeholder="It is still empty here"
+            value={diaryText}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DiaryEntryContainer;

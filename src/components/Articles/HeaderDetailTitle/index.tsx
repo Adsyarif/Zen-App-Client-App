afa7d7c9d3f2c2a20de38c1e1a849efd96e066df
Orang9 <egbert.fewi@gmail.com>
2024-08-12 20:22:37 +0700

    feat: add feature for header detail article

---
+++ src/components/Articles/HeaderDetailTitle/index.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { RxDotFilled } from "react-icons/rx";
+
+export default function HeaderDetailTitle(
+  { article }: { article: any }
+) {
+  return (
+    <>
+      <p className="text-3xl font-semibold">Featured</p>
+      <h2 className="text-4xl font-bold">{article.title}</h2>
+      <div className="flex justify-start items-center">
+        <Image
+          src={"/counselorImg.png"}
+          width={320}
+          height={120}
+          alt={`Profile picture of ${article.author}`}
+          className="w-10 h-10 p-1 rounded-full object-cover object-center"
+        />
+        <h3 className="flex text-base font-semibold">
+          Publisher <RxDotFilled /> 31 July 2024
+        </h3>
+      </div>
+    </>
+  );
+}

f0cfcf6eb3199aa5906c7f9d34cf32c52a2100ee
Orang9 <egbert.fewi@gmail.com>
2024-08-12 20:15:44 +0700

    feat: change header image

--- src/components/Articles/HeaderImageArticles/index.tsx
+++ src/components/Articles/HeaderImageArticles/index.tsx
@@ -12,9 +12,9 @@ export default function HeaderImageArticles() {
 
       <div className="absolute bottom-0 w-full flex justify-center items-center">
         <div className="bg-black bg-opacity-10 text-white p-4 lg:p-20 rounded-lg">
-            <h1 className="text-xl md:text-2xl lg:text-3xl lg:py-2">Featured</h1>
-            <h2 className="text-base md:text-xl font-bold lg:text-2xl lg:py-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, vel. Voluptates nobis magni eaque facere provident ex consequatur accusantium.</h2>
-            <p className="text-sm md:text-lg lg:text-xl lg:py-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi maxime fugit, beatae culpa voluptatibus asperiores et!</p>
+            <h1 className="text-lg md:text-2xl lg:text-3xl lg:py-2">Featured</h1>
+            <h2 className="text-sm md:text-xl font-bold lg:text-2xl lg:py-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, vel. Voluptates nobis magni eaque facere provident ex consequatur accusantium.</h2>
+            <p className="text-xs md:text-lg lg:text-xl lg:py-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi maxime fugit, beatae culpa voluptatibus asperiores et!</p>
         </div>
       </div>
     </div>

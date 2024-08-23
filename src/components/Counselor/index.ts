6c02c6016f07964ea2aafc9aa3818bb40f191481
desyasarbini <desya.sarbini@gmail.com>
2024-08-23 19:39:41 +0800

    feat: create card counselor

--- src/components/Counselor/index.ts
+++ src/components/Counselor/index.ts
@@ -1,3 +1,4 @@
 export { default as HeaderCounselor } from "./HeaderCounselor";
 export { default as CounselorCard } from "./CounselorCard";
 export { default as Pagination } from "./Pagination";
+export { default as CounselorProfileCard } from "./CounselorProfileCard";

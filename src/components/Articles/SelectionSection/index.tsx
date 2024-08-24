6904ede729b454ba63c686abf10d5dc543c0864b
Orang9 <egbert.fewi@gmail.com>
2024-08-24 19:13:35 +0700

    refactor: adapting the Selection section page

--- src/components/Articles/SelectionSection/index.tsx
+++ src/components/Articles/SelectionSection/index.tsx
@@ -7,7 +7,7 @@ export default function SelectionSection({ article }: { article: any }) {
             <li key={index} className="flex items-center group cursor-pointer">
               <div className="h-8 w-1 bg-gray-300 group-hover:bg-teal-600 mr-2"></div>
               <h3 className="text-lg font-semibold group-hover:text-teal-600">
-                {section.sub_title}
+                {section.sub_bab}
               </h3>
             </li>
           ))}

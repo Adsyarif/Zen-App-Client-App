17c1a256ba393e2a388741e00cd100dd07559c04
Orang9 <egbert.fewi@gmail.com>
2024-08-30 14:36:16 +0700

    refactor: diary entry container style

--- src/components/diary_entry_container.tsx
+++ src/components/diary_entry_container.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { use, useState } from "react";
 import ZenZoneBrandLogo from "../../public/ZenZoneBrandPict.png";
 import DropdownMoodPicker from "./dropdown_mood_picker";
 import DropdownTripleDotsMenu from "./dropdown_triple_dots";
-import { Dayjs } from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
+import { FaCalendarAlt } from "react-icons/fa";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
+import { date } from "yup";
 
 interface DiaryEntryContainerProps {
   selectedDate: Dayjs | null;
@@ -11,14 +16,13 @@ interface DiaryEntryContainerProps {
   diaryText: string;
   setDiaryText: (text: string) => void;
   setMoodText: (text: string) => void;
+  setSelectedDate: (date: Dayjs | null) => void;
   onSave: () => void;
   onDelete: () => void;
   onEdit: () => void;
-  onShare?:()=> void;
+  onShare?: () => void;
   diaryId: number | null;
 }
-
-
 
 const DiaryEntryContainer: React.FC<DiaryEntryContainerProps> = ({
   selectedDate,
@@ -27,25 +31,46 @@ const DiaryEntryContainer: React.FC<DiaryEntryContainerProps> = ({
   diaryText,
   setMoodText,
   setDiaryText,
+  setSelectedDate,
   onSave,
   onDelete,
   onEdit,
   onShare,
-  diaryId
+  diaryId,
 }) => {
+  const [showDatePicker, setShowDatePicker] = useState(false);
+
   const handleDiaryChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setDiaryText(event.target.value);
   };
 
-  const formattedDate = selectedDate ? selectedDate.format("DD-MMMM-YYYY") : "No date selected";
+  const handelDateChange = (date: Dayjs | null) => {
+    setSelectedDate(date);
+    setShowDatePicker(false);
+  };
+
+  const formattedDate = selectedDate
+    ? selectedDate.format("DD-MMMM-YYYY")
+    : "No date selected";
 
   return (
     <div className="bg-green-900 text-lg font-semibold text-center items-center rounded-lg p-6 h-screen">
-      <p className="text-white text-2xl mb-6">{selectMoodStatus ? "Edit Diary Entry" : "New Diary Entry"}</p>
+      <p className="text-white text-2xl mb-6">
+        {selectMoodStatus ? "Edit Diary Entry" : "New Diary Entry"}
+      </p>
       <div className="bg-gray-100 rounded-lg shadow-md p-4 w-full h-full max-h-[80vh] overflow-y-auto">
-        <div className="flex ">
+        <div className="flex md:hidden justify-end ">
           {/* <DropdownMoodPicker /> */}
-          
+          <FaCalendarAlt
+            size={24}
+            className="text-leaf md:text-2xl"
+            onClick={() => setShowDatePicker(!showDatePicker)}
+          />
+          {showDatePicker && (
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <MobileDatePicker value={selectedDate} onChange={handelDateChange} />
+            </LocalizationProvider>
+          )}
         </div>
         <div className=" flex flex-col ">
           <div className=" flex flex-row justify-between">
@@ -58,7 +83,13 @@ const DiaryEntryContainer: React.FC<DiaryEntryContainerProps> = ({
               </p>
             </div>
             <div className=" flex items-center">
-              <DropdownTripleDotsMenu onSave={onSave} onEdit={onEdit} onDelete={onDelete} onShare={onShare ?? (() => {})} diaryId={diaryId ? diaryId.toString() : ""}  />
+              <DropdownTripleDotsMenu
+                onSave={onSave}
+                onEdit={onEdit}
+                onDelete={onDelete}
+                onShare={onShare ?? (() => {})}
+                diaryId={diaryId ? diaryId.toString() : ""}
+              />
             </div>
           </div>
         </div>

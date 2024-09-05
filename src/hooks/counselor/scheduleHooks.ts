083dfdebd4ff372dbc6ae5903259580b3b99b069
Adsyarif <adrisansyarif@gmail.com>
2024-09-05 14:10:53 +0700

    feat: add entry point to put schedule API

--- src/hooks/counselor/scheduleHooks.ts
+++ src/hooks/counselor/scheduleHooks.ts
@@ -1,5 +1,5 @@
 import { API_BASE } from "@/lib/projectApi";
-import { changeTimeZone } from "@/utils/dateFormated";
+import { changeTimeZone, dateManipulation } from "@/utils/dateFormated";
 import { useEffect, useState } from "react";
 
 export const useAllSchedule = () => {
@@ -34,6 +34,7 @@ export const useAllSchedule = () => {
       counselorId: schedule.counselor_detail.account_id,
       from: changeTimeZone(schedule.available_from, "WIB"),
       to: changeTimeZone(schedule.available_to, "WIB"),
+      status: schedule.status,
     };
   });
 
@@ -46,14 +47,24 @@ export const useIdSchedule = (id: number) => {
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const fetchCounselorData = async () => {
+    const fetchActiveCounselorSchedules = async () => {
+      const date = new Date();
+      const currentTime = dateManipulation(date.toLocaleString());
+      const currentDate = currentTime[0];
       try {
         const response = await fetch(`${API_BASE}/list_schedule/${id}`);
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
         const data = await response.json();
-        setSchedules(data.data);
+        const schedules_list = data.data;
+        const availableSchedulesData = schedules_list.filter(
+          (schedule: any) =>
+            schedule.status === null &&
+            schedule.booked_by_account_id === null &&
+            schedule.available_from >= currentDate
+        );
+        setSchedules(availableSchedulesData);
       } catch (error: any) {
         setError(error.message);
         console.error("Failed to fetch counselor data", error);
@@ -62,13 +73,12 @@ export const useIdSchedule = (id: number) => {
       }
     };
 
-    fetchCounselorData();
+    fetchActiveCounselorSchedules();
   }, []);
 
   const listSchedules = schedules.map((schedule: any) => {
     return {
       bookID: schedule.schedule_id,
-      patientId: schedule.counselor_id,
       counselorId: schedule.counselor_detail.account_id,
       from: changeTimeZone(schedule.available_from, "WIB"),
       to: changeTimeZone(schedule.available_to, "WIB"),

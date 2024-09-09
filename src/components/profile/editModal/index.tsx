f6e939e7cf8aea1f95ac0d6e69296f75e52e52e5
smmuja <smmuja27@gmail.com>
2024-09-10 00:16:20 +0700

    fix(edit profile): gender select

--- src/components/profile/editModal/index.tsx
+++ src/components/profile/editModal/index.tsx
@@ -1,20 +1,46 @@
+import { API_BASE } from "@/lib/projectApi";
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 
 interface ModalProps {
   isVisible: boolean;
   onClose: () => void;
-  userData: any; 
-  onSave: (updatedData: any) => void; 
+  userData: any;
+  onSave: (updatedData: any) => void;
 }
 
-const EditModal: React.FC<ModalProps> = ({ isVisible, onClose, userData, onSave }) => {
+interface genderProps {
+  gender_id: number;
+  name: string;
+}
+
+const EditModal: React.FC<ModalProps> = ({
+  isVisible,
+  onClose,
+  userData,
+  onSave,
+}) => {
   const [formData, setFormData] = useState({
     first_name: "",
     last_name: "",
     user_name: "",
     phone_number: "",
-    gender_name: "",
+    gender_id: "",
   });
+
+  const [genderCategories, setgenderCategories] = useState<genderProps[]>([]);
+
+  useEffect(() => {
+    const fetchGenders = async () => {
+      try {
+        const response = await axios.get(`${API_BASE}/gender`);
+        setgenderCategories(response.data.data);
+      } catch (error) {
+        console.error("Error fetching genders:", error);
+      }
+    };
+    fetchGenders();
+  }, []);
 
   useEffect(() => {
     if (userData) {
@@ -23,12 +49,14 @@ const EditModal: React.FC<ModalProps> = ({ isVisible, onClose, userData, onSave 
         last_name: userData.last_name || "",
         user_name: userData.user_name || "",
         phone_number: userData.phone_number || "",
-        gender_name: userData.gender_name || "",
+        gender_id: userData.gender_id || "",
       });
     }
   }, [userData]);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = e.target;
     setFormData((prev) => ({
       ...prev,
@@ -40,12 +68,15 @@ const EditModal: React.FC<ModalProps> = ({ isVisible, onClose, userData, onSave 
     onSave(formData);
   };
 
+  if (!genderCategories) return;
   if (!isVisible) return null;
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
       <div className="bg-teal-900 p-6 rounded-md md:w-1/2 lg:w-1/4 w-full mx-12">
-        <h2 className="text-xl font-semibold mb-4 text-white text-center">Edit Profile</h2>
+        <h2 className="text-xl font-semibold mb-4 text-white text-center">
+          Edit Profile
+        </h2>
         <div className="">
           <p className="text-white lg:text-xl font-semibold">First Name</p>
           <input
@@ -92,14 +123,23 @@ const EditModal: React.FC<ModalProps> = ({ isVisible, onClose, userData, onSave 
         </div>
         <div>
           <p className="text-white lg:text-xl font-semibold">Gender</p>
-          <input
-            type="text"
-            name="gender_name"
-            value={formData.gender_name}
+
+          <select
+            name="gender_id"
+            value={formData.gender_id}
+            required
             onChange={handleChange}
-            placeholder="Add text in here..."
             className="capitalize w-full p-2 border border-gray-300 rounded mb-4"
-          />
+          >
+            <option defaultValue="Select Gender" disabled>
+              Select Gender
+            </option>
+            {genderCategories.map((gender) => (
+              <option key={gender.gender_id} value={gender.gender_id}>
+                {gender.name}
+              </option>
+            ))}
+          </select>
         </div>
         <div className="flex justify-center">
           <button
